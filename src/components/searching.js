4f44dbf7d440673a5de35221c74e3c0d1d8e6bfe
Marta Kozhevina <martasabantseva@yandex.ru>
2026-03-17 12:31:30 +0100

    done

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,14 @@
 import {rules, createComparison} from "../lib/compare.js";
 
-
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+    const compare = createComparison(
+        ['skipEmptyTargetValues'], // Стандартные правила по именам
+        [rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)] // Кастомное правило
+    );
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
-    }
+        return data.filter(item => compare(item, state));
+    };
 }
